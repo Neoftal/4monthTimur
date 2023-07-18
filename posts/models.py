a103a6f9346eefe54ab9edb341012a2665269f50
Пользователь <neoftality@gmail.com>
2023-07-18 16:45:40 +0600

    hw2

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
+
+
+# Create your models here.
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
